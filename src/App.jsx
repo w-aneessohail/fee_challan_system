4040984c91ffc3c406169d8d877067a9533c837c
Anees Sohail <work.aneessohail@gmail.com>
2026-04-09 13:01:27 +0500

    Initial commit: moved project to its own folder

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <div className="h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-4xl font-bold text-green-400">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
